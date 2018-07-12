225a4a0023dc881fdaaa8010e2822596f88010d1
Adam Giese <adam.giese@underarmour.com>
2018-07-12 14:17:30 -0500

    Add formatting to surprise command

--- messages/surprise.js
+++ messages/surprise.js
@@ -4,10 +4,19 @@ const location = require('../shared/location');
 const surprise = (request, response) => {
   zomato.get(`search?lat=${location.lat}&lon=${location.lon}&radius=1610&count=25&sort=real_distance`)
     .then(({ data }) => {
-      const randomRestro = data.restaurants[Math.floor(Math.random() * data.restaurants.length)];
+      const restaurant = data.restaurants[Math.floor(Math.random() * data.restaurants.length)].restaurant;
       return response.status(200)
         .send({
-          surprise: randomRestro.restaurant.name,
+          "attachments": [
+            {
+              "color": "FF0080",
+              "title": restaurant.name,
+              "title_link": restaurant.url,
+              "text": restaurant.location.address,
+              "footer": "Chosen at Random!",
+              "thumb_url": restaurant.thumb,
+            },
+          ],
         });
     })
     .catch(error => response.send({ error }));
