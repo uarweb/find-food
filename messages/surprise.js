6c9bbcb5c1a102b50af6b82a933c24593277d9e3
Adam Giese <adam.giese@underarmour.com>
2018-07-12 15:27:06 -0500

    Add filtering to surprises

--- messages/surprise.js
+++ messages/surprise.js
@@ -1,10 +1,32 @@
+const querystring = require('querystring');
 const zomato = require('../shared/zomatoApi.js');
 const location = require('../shared/location');
 
 const surprise = (request, response) => {
-  zomato.get(`search?lat=${location.lat}&lon=${location.lon}&radius=1610&count=25&sort=real_distance`)
-    .then(({ data }) => {
-      const restaurant = data.restaurants[Math.floor(Math.random() * data.restaurants.length)].restaurant;
+
+  const sharedParams = {
+    lat: location.lat,
+    lon: location.lon,
+    radius: 1610,
+    count: 20,
+    sort: 'real_distance',
+  }
+
+  const getRestaurantsWithOffset = offset => zomato.get(`search?${querystring.stringify({...sharedParams, start: offset})}`);
+
+  Promise.all([
+    getRestaurantsWithOffset(0),
+    getRestaurantsWithOffset(20),
+    getRestaurantsWithOffset(40),
+  ]).then(([ ...responses ]) => {
+      const unfilteredRestaurants = responses.reduce((list, response) => [...list, ...response.data.restaurants], []);
+
+      const blacklistedCuisines = ['Drinks Only', 'Bar Food', 'Cafe', 'Ice Cream'];
+      const outBlacklistedCuisines = location => !blacklistedCuisines.includes(location.restaurant.cuisines);
+
+      const restaurants = unfilteredRestaurants.filter(outBlacklistedCuisines);
+      const restaurant = restaurants[Math.floor(Math.random() * restaurants.length)].restaurant;
+
       return response.status(200)
         .send({
           "response_type": "in_channel",
